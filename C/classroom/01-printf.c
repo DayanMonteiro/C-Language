32a934b6379eff8e645932f57547c82d01514960
DayanMonteiro <dayan.monteiro@fdte.io>
2022-09-20 17:31:30 -0300

    more classes

--- C/classroom/01-printf.c
+++ C/classroom/01-printf.c
@@ -4,4 +4,4 @@ int main() {
     // Vamos comprar uma no Natal
     printf("No Natal vou me dar de presete\numa guitarra telecaster Fender.\n");
     return 0;
-}
+} 
---
+++ C/classroom/14-conditional-operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+  int num1 = 10;
+  int num2 = 20;
+  int num3 = 100;
+  int num4 = 50;
+
+  num1 < num2 ? printf("Sim\n") : printf("Não\n");
+
+  num3 < num4 ? printf("Sim\n") : printf("Não\n");
+
+  return 0;
+}
